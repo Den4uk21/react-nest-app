f980d82c0302704fbcb117516f49b30d79c2267e
Den4uk21 <den042110@gmail.com>
2020-05-21 19:57:29 +0300

    server settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+import { configModule } from './configure.root'
 
 @Module({
-  imports: []
+  imports: [
+    configModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.USERNAME_DB,
+      password: process.env.PASSWORD_DB,
+      database: 'postgresDB',
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+  ]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const app = await NestFactory.create(AppModule, { cors: true })
+  app.useGlobalPipes(new ValidationPipe())
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('React-Nest-App')
+    .setDescription('The react-nest-app API description')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(3001)
 }
+
 bootstrap()
